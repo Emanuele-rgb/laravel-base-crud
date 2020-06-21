7df6290c031aa71dc9bd54fc2e982a8338ffa7ee
Emanuele Sanquirico <manu.sanquirico@gmail.com>
2020-06-21 22:46:30 +0200

    Seeding working

--- database/seeds/StudentsTableSeeder.php
+++ database/seeds/StudentsTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Student;
+use Faker\Generator as Faker;
 
 class StudentsTableSeeder extends Seeder
 {
@@ -9,27 +11,50 @@ class StudentsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        $students = [
-         [
-            'name'=>'Emanuele Sanquirico',
-            'gender'=>'m',
-            'description'=>'Its meeeee',
-            'age'=>'22'
-         ],
-         [
-            'name'=>'Gianni Verdi',
-            'gender'=>'m',
-            'description'=>'Its GIANNIII',
-            'age'=>'22'
-         ],
-         [
-            'name'=>'Mario Giallo',
-            'gender'=>'m',
-            'description'=>'Its MARIO',
-            'age'=>'22'
-         ]
-         ];
+        // $students = [
+        //  [
+        //     'name'=>'Emanuele Sanquirico',
+        //     'gender'=>'m',
+        //     'description'=>'Its meeeee',
+        //     'age'=>'22'
+        //  ],
+        //  [
+        //     'name'=>'Gianni Verdi',
+        //     'gender'=>'m',
+        //     'description'=>'Its GIANNIII',
+        //     'age'=>'22'
+        //  ],
+        //  [
+        //     'name'=>'Mario Giallo',
+        //     'gender'=>'m',
+        //     'description'=>'Its MARIO',
+        //     'age'=>'22'
+        //  ]
+        //  ];
+        //  foreach ($students as $student){
+        //      $newStudent = new Student();
+        //      $newStudent->fill($student);
+        //      $newStudent->save();
+        //  } 
+        Student::truncate();
+
+        $records = 10;
+
+        for ($i=0; $i < $records; $i++) { 
+
+            $gender = $faker->randomElement(['male', 'female']);
+            $genderShort = ($gender == 'male') ? 'm' : 'f';
+
+            $newStudent = new Student();
+
+            $newStudent->name = $faker->name($gender);
+            $newStudent->gender = $genderShort;
+            $newStudent->description = $faker->paragraph(3, true);
+            $newStudent->age = $faker->numberBetween(12,50);
+
+            $newStudent->save();
+        }
   }
 }
